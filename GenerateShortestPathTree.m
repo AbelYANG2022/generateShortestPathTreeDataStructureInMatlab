5509575e1b817b0e5101f97bffa3ced28de725f2
FactorYang <yangze@hku.hk>
2015-10-23 11:48:06 +0800

    Update GenerateShortestPathTree.m

--- GenerateShortestPathTree.m
+++ GenerateShortestPathTree.m
@@ -1,4 +1,15 @@
+
+
 function [t] = GenerateShortestPathTree(root, t_edge)
+%% tree data structure generator function
+%% input: 
+    %root: the root node | data type: cell | eg. {'1'}
+    %t_edge: the shortest path data structure | data type: digraph | eg.
+        %t_edge.Edges = table: '2' '1'; '2' '0'  
+%% output:
+    %t: the tree data structure | dta type: tree
+    
+%%
     t = tree(root);%node is cell format
     tem = t_edge.Edges{:,:}(:,1);
     indexChild = find(str2num(cell2mat(tem(:, 1))) == str2num(cell2mat(root(1))));% find the edge start with root
