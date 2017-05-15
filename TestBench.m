120ffcc168009997f2180261e1e7240de3cb106c
FactorYang <yangze@hku.hk>
2017-05-15 16:57:27 +0800

    Update TestBench.m

--- TestBench.m
+++ TestBench.m
@@ -1,7 +1,7 @@
-s = {'1', '1', '2', '2', '3', '4', '4', '5', '5', '6', '7', '8', '9'};
-t = {'2', '10', '10', '3', '4', '5', '9', '6', '8', '7', '8', '9', '10'};
+s = [1, 1, 2, 2, 3, 4, 4, 5, 5, 6, 7, 8, 9]; % source of each link
+t = [2, 10, 10, 3, 4, 5, 9, 6, 8, 7, 8, 9, 10]; % destination of each link
 
-root = {'3'};
+root = 3; % tree rooted at node 3
 G = graph(s, t);
 t_edge = shortestpathtree(G, root);%generate the all the shortest path edges on the tree
 plot(t_edge);
